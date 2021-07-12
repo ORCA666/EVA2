2fa4662f03645af1ab3d5ae5248896749c28f64e
ORCA666 <mohammadalisareeni@gmail.com>
2021-07-12 08:48:34 +0300

    Add files via upload

---
+++ EVA2-/syscalls.asm
@@ -0,0 +1,77 @@
+.code
+
+EXTERN SW2_GetSyscallNumber: PROC
+
+NtOpenProcess PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 0E6BEC114h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtOpenProcess ENDP
+
+NtAllocateVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 00D967173h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtAllocateVirtualMemory ENDP
+
+NtWriteVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 00BDF1945h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtWriteVirtualMemory ENDP
+
+NtCreateThreadEx PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 014AB5E76h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtCreateThreadEx ENDP
+
+end
